c971cd5d7b1811af5713af1c9f423cbc392460ad
Abner Matheus <abnerm80@gmail.com>
2020-02-21 17:59:56 -0300

    Atualizando Login

--- script/login.js
+++ script/login.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-            $("#form").submit(function() {
-                var nome = $("#nome").val();
-                var ultimoNome = $("#sobrenome").val();
-                var senha = $("#senha").val();
-                var repetirsenha = $("repetirsenha").val();
-                if (confirma_senha == senha) {};
-            })
+
+
+
+
+})
