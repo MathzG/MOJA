22780e009d139f37e7452275ed3bd515604bc8d2
Abner Matheus <abnerm80@gmail.com>
2020-02-20 22:34:37 -0300

    Commit Abner 5

--- script/login.js
+++ script/login.js
@@ -1,9 +1,8 @@
-$(document).ready(function(){
-    $("#form").submit(function(){
-        var nome = $("#nome").val();
-        var ultimoNome = $("#sobrenome").val();
-        var senha = $("#senha").val();
-        var repetirsenha = $("repetirsenha").val();
-        if(confirma_senha == senha){
-    })
-})
+$(document).ready(function() {
+            $("#form").submit(function() {
+                var nome = $("#nome").val();
+                var ultimoNome = $("#sobrenome").val();
+                var senha = $("#senha").val();
+                var repetirsenha = $("repetirsenha").val();
+                if (confirma_senha == senha) {};
+            })
