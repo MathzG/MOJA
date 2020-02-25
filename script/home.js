2927a76bdf53e3cb2a3640d4fb574832dcc702a3
Abner Matheus <abnerm80@gmail.com>
2020-02-24 21:17:10 -0300

    Atualizando Home

--- script/home.js
+++ script/home.js
@@ -3,9 +3,28 @@ $(document).ready(function() {
         draggable: true,
 
     });
+    $('.carousel').carousel({
+        dist: 0,
+        padding: 0,
+        fullWidth: true,
+        indicators: true,
+        duration: 100,
+    });
+    // CAROUSEL
+
+
+    autoplay()
+
 
 
 
 
 
 });
+
+
+
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
