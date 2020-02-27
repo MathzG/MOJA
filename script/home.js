80feade97bcaa2e235c33faa6bbd5f435734fa7c
Abner Matheus <abnerm80@gmail.com>
2020-02-27 18:33:01 -0300

    Atualizando All

--- script/home.js
+++ script/home.js
@@ -9,11 +9,22 @@ $(document).ready(function() {
         fullWidth: true,
         indicators: true,
         duration: 150,
+        
     });
     // CAROUSEL
 
 
     autoplay()
+
+   /* $(document).scroll(function(){
+        var windowTop = $(window).scrollTop();
+        if(windowTop>50){
+            $('#nav').addClass('efect-nav');
+        }
+        else{
+            $('#nav').removeClass('efect-nav');
+        }
+    })*/
 
 
 
